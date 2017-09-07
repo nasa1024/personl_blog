8549b7c3a9efdfe633c1e92df0fd9993a901d745
lih <541573560@qq.com>
2017-09-07 21:16:28 +0800

    aaaaa

--- personl_blog/wsgi.py
+++ personl_blog/wsgi.py
@@ -9,8 +9,24 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
+from os.path import join, dirname, abspath
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personl_blog.settings")
+
+application = get_wsgi_application()
+
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))  # 3
+
+sys.path.insert(0, PROJECT_DIR)  # 5
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "blog.settings"  # 7
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()

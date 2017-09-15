2a3e5d7e50da93920fa725519cb036b1666b6c5b
lih <541573560@qq.com>
2017-09-15 21:38:16 +0800

    changge the models and a_page.html

--- blog/models.py
+++ blog/models.py
@@ -4,20 +4,13 @@ from django.db import models
 
 
 class Author_massage(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(u'姓名',max_length=50)
     join_date = models.DateField(u'加入日期', auto_now_add=True, editable = True)
 #    head_portrait = models.ImageField()
     self_introduce = models.CharField(u'个性签名',max_length=240)
 
     def __str__(self):
         return self.name
-
-    def my_property(self):
-        return self.name + '  ' + self.self_introduce
-    my_property.short_description = "author_massage"
-
-    person_introduce = property(my_property)
-
 
 class Artical(models.Model):
     author = models.ForeignKey(Author_massage)

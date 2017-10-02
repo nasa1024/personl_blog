fcc4e6ff3df8e1d5e3e2113d47757befa53a9f86
lih <541573560@qq.com>
2017-10-02 15:42:24 +0800

    增加留言功能“ ”

--- gustbook/models.py
+++ gustbook/models.py
@@ -4,7 +4,8 @@ class words_board(models.Model):
     username = models.CharField(max_length=30)
     content = models.TextField()
     publish = models.DateTimeField(auto_now_add=True)
+    e_mail = models.CharField(max_length=52)
 
     def __str__(self):
-        tpl = '<Message:[username={username}, content={content}, publish={publish}]>'
-        return tpl.format(username=self.username, content=self.content, publish=self.publish)
+        tpl = '<Message:[username={username}, content={content}, publish={publish}>'
+        return tpl.format(username=self.username, content=self.content, publish=self.publish, email =self.e_mail)
--- gustbook/views.py
+++ gustbook/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from gustbook.models import words_board
+from django.shortcuts import HttpResponseRedirect
 
 def index(request):
     message = words_board.objects.all()
-    return render(request,'index.html',{'messages':message})
+    return render(request,'index1.html',{'messages':message})
 
 def save(request):
     username = request.POST.get("username")
-    #title = request.POST.get("title")
+    email = request.POST.get("email")
     content = request.POST.get("content")
  #   publish = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = words_board(content=content, username=username)
+    message = words_board(content=content, username=username, e_mail=email)
     message.save()
 
-    #return HttpResponse('/gustbook/index/')
-    return render(request,'save.html')
+    return HttpResponseRedirect('/gustbook/index/')
 
 def create(request):
     return render(request, 'create.html')
